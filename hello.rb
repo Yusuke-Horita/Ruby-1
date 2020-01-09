ebd9edb0a0dcee314bb8d5bd2e9b32fac35ad504
Yusuke-Horita <somagawa645@gmail.com>
2020-01-09 23:45:15 +0900

    “Rails復習”

--- hello.rb
+++ hello.rb
@@ -1,3 +1,14 @@
-colors = ["red", "blue", "yellow"]
+class User
 
-colors.push
+	def initialize(name)
+		@name = name
+	end
+
+	def sayHi
+		puts "hi! I am #{@name}"
+	end
+end
+
+
+tom = User.new("tom")
+tom.sayHi
